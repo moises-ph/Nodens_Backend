512da884c0c9100161dbbf03bf57ead558de93b9
ethiemGuerrero <ethiemalex03@gmail.com>
2023-02-04 13:04:50 -0500

    register

---
+++ Users_Auth/source/MS_Users_Auth/MS_Users_Auth/Controllers/UserController.cs
@@ -0,0 +1,66 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MS_Users_Auth.Models;
+
+namespace MS_Users_Auth.Controllers
+{
+    [EnableCors("ReglasCors")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly string cadenaSQL;
+        public UserController(IConfiguration config)
+        {
+            cadenaSQL = config.GetConnectionString("cadenaSQL");
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public IActionResult Register([FromBody] User obj)
+        {
+            try
+            {
+                int error;
+                string Response;
+                using (var conexion = new SqlConnection(cadenaSQL))
+                {
+                    conexion.Open();
+                    var cmd = new SqlCommand("SP_CreateUser", conexion);
+                    cmd.Parameters.AddWithValue("Email", obj.Email);
+                    cmd.Parameters.AddWithValue("First_Name", obj.First_Name);
+                    cmd.Parameters.AddWithValue("Second_Name", obj.Second_Name);
+                    cmd.Parameters.AddWithValue("First_Lastname", obj.First_Lastname);
+                    cmd.Parameters.AddWithValue("Second_Lastname", obj.Second_Lastname);
+                    cmd.Parameters.AddWithValue("Rol", obj.Rol);
+                    cmd.Parameters.AddWithValue("Birthdate", obj.Birthdate);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+                    using (SqlDataReader rd = cmd.ExecuteReader())
+                    {
+                        rd.Read();
+                        error = Convert.ToInt32(rd["Error"].ToString());
+                        Response = rd["Message"].ToString();
+                        rd.Close();
+                    }
+                    conexion.Close();
+                }
+                if (error == 1)
+                {
+                    throw new Exception(Response);
+                }
+                else
+                {
+                    return StatusCode(StatusCodes.Status200OK, new { Message = Response });
+                }
+            }
+            catch(Exception err)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { Message = err.Message });
+            }
+        }
+    }
+}
