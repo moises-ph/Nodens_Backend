3b0420c03281886d6ccda5fb4a85bd2c37e85d67
BrayanMedinaMoreno <brayan0519@yahoo.com>
2023-03-17 21:26:11 -0500

    XD

--- Musicians/Service/src/app.py
+++ Musicians/Service/src/app.py
@@ -1,17 +1,24 @@
 #reunir el codigo y iniciar el svr
 from controllers import musiciansController
 from flask import Flask, redirect, url_for
+from flask_optional_routes import OptionalRoutes
 
 app = Flask(__name__)
 
-#app.add_url_rule('/<email>',"index", musiciansController.getInfomusician, methods=["GET"])
-"""
-@app.route('/musician', methods=["GET"])
-def musician():
-    return redirect(url_for('index'))
-"""
+optional = OptionalRoutes(app)
+
 ### GET ###
+app.add_url_rule('/one/<id>',"onlyinfo", musiciansController.getMusician, methods=["GET"])
+### POST ###
+app.add_url_rule('/new',"postMusician", musiciansController.postInfomusician, methods=["POST"])
+### DELETE ###
+app.add_url_rule('/drop/<id>',"deleteMusician", musiciansController.deleteMusician, methods=["DELETE"])
+### PUT ###
+app.add_url_rule('/update/<id>',"updateMusician", musiciansController.putMusician, methods=["PUT"])
 #
+
+### GET ###
+"""
 @app.route('/', methods=["GET"])
 def home():
     return musiciansController.home()
@@ -23,18 +30,24 @@ def getmusicians():
 @app.route("/musician", methods=["POST"])
 def addMusician():
     return musiciansController.postInfomusician()
-#
-
 
 """
-@app.route('/musician/<option>')
+
+
+@optional.routes('/musician/<option>/<id>?', methods=["POST","GET","DELETE","PUT"])
 def musicians(option):
     print(option)
-    match option:
-        case "index":
-            print(url_for('index'))
-            return redirect(url_for('index'))        
-"""
+    print(id)
+    match option:      
+        case "update":
+            return redirect(url_for("updateMusician",id=id))
+        case "one":
+            return redirect(url_for('onlyinfo',id=id))
+        case "drop":
+            return redirect(url_for('deleteMusician',id=id))
+        case "new":
+            return redirect(url_for('postMusician',_method="POST"))
+
 
 
 
--- Musicians/Service/src/controllers/musiciansController.py
+++ Musicians/Service/src/controllers/musiciansController.py
@@ -1,6 +1,7 @@
 from flask import jsonify, request, url_for,redirect,Response
 from Database import db as Database
 from bson import json_util
+from bson.objectid import  ObjectId
 from validations.MusicianInfo import musicianinfo as infoMusician
 
 ### ========> Solucionar errores <======== ###
@@ -9,24 +10,34 @@ from validations.MusicianInfo import musicianinfo as infoMusician
 # iniciar hacer delete y put
 # linea 40
 
-
-
 # Musician
+"""
 def getMusician(email):
     db = Database.dbConnection()
     musicians = list(db.Musicians.find_one({"correo" : email}))
     return jsonify({musicians})
 
+"""
 def home():
     return jsonify({"Message":"pong"})
-### GET ###
+### GET ALL ###
 def getInfomusician ():
     db = Database.dbConnection()
-    users = list(db.users.find())
+    users = list(db.Musicians.find())
     response = json_util.dumps(users)
-    return Response(response, mimetype="application/json")
-
-
+    return response #Response(response, mimetype="application/json")
+### GET ONLY ID ###
+def getMusician(id):
+    db = Database.dbConnection()
+    user = db.Musicians.find_one({"_id" : ObjectId(id)})
+    response = json_util.dumps(user)
+    return response
+### DELETE ###   
+def deleteMusician(id):
+    db = Database.dbConnection()
+    db.Musicians.delete_one({"_id": ObjectId(id)})
+    response = jsonify({"message": "user" + id + "was deleted successfully"})
+    return response
 ### POST ###
 def postInfomusician():
     # recive datos
@@ -37,11 +48,11 @@ def postInfomusician():
     if username and email and years:
         db = Database.dbConnection()
 
-        id = db.users.insert_one(
+        id = db.Musicians.insert_one(
             {"username": username,"email": email,"years": years}
         )
         response = {
-            "id": str(id),
+            "id": str(id.inserted_id),
             "username": username,
             "years": years,
             "email": email
@@ -49,6 +60,20 @@ def postInfomusician():
         return response
     else:
         return not_Found()
+### PUT ###
+def putMusician (id):
+    username = request.json["username"]
+    years = request.json["years"]
+    email = request.json["email"]
+    if username and years and email:
+        db = Database.dbConnection()
+        db.Musicians.update_one({"_id": ObjectId(id)}, {"$set": {
+            "username" : username,
+            "years" : years,
+            "email" : email    
+        }})
+        responde = jsonify({"message": "user" + id + "was updated successsfully"})
+        return responde
     
 """
 ### method put ###
