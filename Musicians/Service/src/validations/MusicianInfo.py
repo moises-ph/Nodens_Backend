6ec3c8c276456d0ee157f2f02641d529fd8a86a5
BrayanMedinaMoreno <brayan0519@yahoo.com>
2023-03-18 13:50:01 -0500

    as

--- Musicians/Service/src/validations/MusicianInfo.py
+++ Musicians/Service/src/validations/MusicianInfo.py
@@ -1,20 +1,14 @@
 from wtforms import (StringField, TextAreaField, IntegerField, BooleanField,
                      RadioField)
 from wtforms.validators import InputRequired, Length
+from flask_wtf import FlaskForm
 
-class musicianInstrument:
-    def __init__(self) -> None:
-        pass
-
-class musicianinfo:
-    def __init__(self, name, email, years):
-        self.name = name
-        self.email = email
-        self.years = years
-        
-    def toDBCollection(self):
-        return{
-            "name": self.name,
-            "email": self.email,
-            "years": self.years
-        }
+class musicianInstrument(FlaskForm):
+    _id = StringField("id", validators=[InputRequired(),
+                                        ])
+    fecha_nacimiento = StringField("date", validators=[InputRequired(),
+                                                       Length(min=8, max=10)])
+    email = StringField("email", validators=[InputRequired(),
+                                             Length(min=8
+                                                    )])
+print (len("64151854e036763b1c55896d"))
