30a3567fb404a407194ec2051878032f0e98d26a
moisesph <mois.mp8@gmail.com>
2023-03-23 11:33:36 -0500

    Empezar con los organizadores

--- Musicians/Service/src/validations/MusicianInfo.py
+++ Musicians/Service/src/validations/MusicianInfo.py
@@ -1,37 +1,37 @@
 from marshmallow import Schema, fields
 
 class InstrumentsField(Schema):
-    nombre = fields.String()
-    nivel = fields.String()
+    nombre = fields.String(required=True)
+    nivel = fields.String(required=True)
 
 class EducacionField(Schema):
-    nombre = fields.String()
-    institucion = fields.String()
-    fecha_inicio = fields.Date()
-    fecha_fin = fields.Date()
+    nombre = fields.String(required=True)
+    institucion = fields.String(required=True)
+    fecha_inicio = fields.Date(required=True)
+    fecha_fin = fields.Date(required=True)
 
 class RedesSocialesField(Schema):
-    nombre = fields.String()
-    url = fields.String()
+    nombre = fields.String(required=True)
+    url = fields.String(required=True)
 
 class MusicianSchemaCreate(Schema):
-    IdAuth = fields.Integer()
-    fecha_nacimiento = fields.Date()
-    instrumentos = fields.List(fields.Dict())
-    generos = fields.List(fields.String()) 
-    pais = fields.String()
-    ciudad = fields.String()
-    experiencia = fields.String()
-    educacion = fields.List(fields.Dict())
-    url_foto_perfil = fields.String()
-    url_video_presentacion = fields.String()
-    redes_sociales = fields.List(fields.Dict())
+    IdAuth = fields.Integer(required=True)
+    fecha_nacimiento = fields.Date(required=True)
+    instrumentos = fields.List(fields.Dict(),required=True)
+    generosMusicales = fields.List(fields.String(),required=True)
+    pais = fields.String(required=True)
+    ciudad = fields.String(required=True)
+    experiencia = fields.String(required=True)
+    educacion = fields.List(fields.Dict(),required=True)
+    url_foto_perfil = fields.String(required=True)
+    url_video_presentacion = fields.String(required=True)
+    redes_sociales = fields.List(fields.Dict(),required=True)
 
 class MusicianSchemaUpdate(Schema):
     IdAuth = fields.Integer(required=True)
     fecha_nacimiento = fields.Date(required=False)
     instrumentos = fields.List(fields.Dict(), required=False)
-    generos = fields.List(fields.String(), required=False) 
+    generosMusicales = fields.List(fields.String(), required=False) 
     pais = fields.String(required=False)
     ciudad = fields.String(required=False)
     experiencia = fields.String(required=False)
